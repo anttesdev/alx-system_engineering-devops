477258247461faad1a6974bce1c730638bc231b5
Anteneh Tesfaye <anttesdev@gmail.com>
2024-02-27 23:02:49 +0300

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts File.read(ARGV[0]).scan(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/).map(&:join)
+
+matches = ARGV[0].match(/from:(?<sender>\+*\w+)\].*to:(?<receiver>\+*\w+)\].*flags:(?<flags>.*?)\]/)
+if matches
+  sender = matches[:sender]
+  receiver = matches[:receiver]
+  flags = matches[:flags]
+  puts "#{sender},#{receiver},#{flags}"
+end
