40b19ceec9ce09b9ce88c40855b1cae910bc1ba9
Anteneh Tesfaye <anttesdev@gmail.com>
2024-04-22 03:32:27 +0300

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# make sure that SSH client configuration refuse to authenticate using a password
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+# make sure that SSH client configuration specifies the private key for authentication
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
