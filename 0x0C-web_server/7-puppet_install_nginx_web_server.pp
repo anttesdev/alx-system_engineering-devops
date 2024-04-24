cfcca468f0c1043ceb3d7cd83f085f626f38fc67
Anteneh Tesfaye <anttesdev@gmail.com>
2024-04-24 22:07:20 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,7 +12,10 @@ service { 'nginx':
 
 # Define the redirection using sed
 exec { 'configure_redirect':
-  command     => "/bin/sed -i 's/server_name _;/server_name _;\\n\\trewrite ^\\/redirect_me https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4 permanent;/' /etc/nginx/sites-enabled/default",
+  command => "/bin/sed -i 's/server_name _;/ \
+            server_name _;\\n\\trewrite ^\\/redirect_me \
+            https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4 permanent;/' \
+            /etc/nginx/sites-enabled/default",
   path        => '/bin',
   refreshonly => true,
   subscribe   => Package['nginx'],
