c3f6d2c532df12e020ab74972d1f81df02bc3214
Anteneh Tesfaye <anttesdev@gmail.com>
2024-04-24 22:14:33 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,21 +3,17 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+exec { 'install nginx':
+  command => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
   require => Package['nginx'],
 }
 
-# Define the redirection using sed
 exec { 'configure_redirect':
-  command => "/bin/sed -i 's/server_name _;/ \
-            server_name _;\\n\\trewrite ^\\/redirect_me \
+  command => "/bin/sed -i 's/listen 80 default_server;/ \
+            listen 80 default_server;\\n\\trewrite ^\\/redirect_me \
             https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4 permanent;/' \
             /etc/nginx/sites-enabled/default",
   path        => '/bin',
-  refreshonly => true,
-  subscribe   => Package['nginx'],
-  notify      => Service['nginx'],
+  provider    => shell,
 }
