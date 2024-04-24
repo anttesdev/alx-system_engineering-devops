c5e9e587d877c26bb3376e21eeccf8d880ac31f9
Anteneh Tesfaye <anttesdev@gmail.com>
2024-04-24 23:02:03 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,19 @@
 # Install Nginx package
-
 exec { 'install nginx':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
   provider => shell,
   require  => Package['nginx'],
 }
-exec {'Add the hello world':
+
+# Add the hello world index.html
+exec { 'Add the hello world':
   command  => 'echo "Hello World!" > /var/www/html/index.html',
   provider => shell,
 }
 
-
+# Configure redirection
 exec { 'configure_redirect':
-  command  => "/bin/sed -i 's/listen 80 default_server;/ \
-            listen 80 default_server;\\n\\tlocation \/redirect_me \
-            {\\n\\treturn 301 https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4\\n\\t}//' \
-            /etc/nginx/sites-enabled/default",
+  command  => "/bin/sed -i 's/listen 80 default_server;/listen 80 default_server;\\n    location \\/redirect_me {\\n        return 301 https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4;\\n    }\\n\\t/' /etc/nginx/sites-enabled/default",
   path     => '/bin',
   provider => shell,
 }
