5deff527f2c3fd002122c6bd74a4556da22c6e73
Anteneh Tesfaye <anttesdev@gmail.com>
2024-04-25 00:51:15 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,7 @@
-# Install Nginx package
-exec { 'install nginx':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
+
+#install nginx using puppet
+
+exec {'install':
   provider => shell,
-  require  => Package['nginx'],
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-# Add the hello world index.html
-exec { 'Add the hello world':
-  command  => 'echo "Hello World!" > /var/www/html/index.html',
-  provider => shell,
-}
-
-file_line { 'install':
-  ensure => 'present',
-  path   => 'etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => package['nginx'],
-}  
