a3a07cc8181ac86d3daf1740fc4358dd17a28c3f
Anteneh Tesfaye <anttesdev@gmail.com>
2024-05-02 01:15:26 +0300

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,5 +2,5 @@
 
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
