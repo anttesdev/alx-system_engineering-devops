77b6611ac7dda81d3f914feaf2c8bf3674ab6edd
Anteneh Tesfaye <anttesdev@gmail.com>
2024-05-02 01:52:30 +0300

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,36 @@
 #Add a custom header using puppet
 
 exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
+  command  => 'apt-get -y update',
 }
 
 exec {'nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
+  command  => 'apt-get -y install nginx',
+  require  => Exec['update'],
 }
 
-exec {'add header':
-  provider => shell,
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default',
+exec {'allow_http':
+  command  => 'ufw allow Nginx HTTP',
+  require  => Exec['nginx'],
 }
 
-exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => present,
+  content => "# Custom configuration for Nginx\nserver {\n\tserver_name _;\n\tadd_header X-Served-By \$hostname;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\tlisten 80 default_server;\n\terror_page 404 /error.html;\n\tlocation = /error.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}\n}",
+}
+
+file { '/var/www/html/error.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [Exec['nginx'], File['/var/www/html/index.nginx-debian.html'], File['/etc/nginx/sites-enabled/default'], File['/var/www/html/error.html']],
 }
---
+++ 0x0F-load_balancer/new.pp
@@ -0,0 +1,21 @@
+#Add a custom header using puppet
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+}
+
+exec {'nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+}
+
+exec {'add header':
+  provider => shell,
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default',
+}
+
+exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
