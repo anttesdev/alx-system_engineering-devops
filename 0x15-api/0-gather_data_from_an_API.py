773e021e5f44001dc34e1396370b85067b2063ab
Anteneh Tesfaye <anttesdev@gmail.com>
2024-05-20 15:18:47 +0300

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A Python script that, uses a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import requests
+from sys import argv
+
+
+def fetch_employee_todo_list(employee_id):
+    """
+    a function that will fetch the progress of an
+    employees progress
+    """
+
+    url = 'https://jsonplaceholder.typicode.com/todos'
+
+    params = {'userId': employee_id}
+
+    response = requests.get(url, params=params)
+
+    todo_list = response.json()
+
+    employee_name = (
+                     requests.get(
+                         'https://jsonplaceholder.typicode.com/users/{}'
+                         .format(employee_id)).json().get('name')
+                     )
+
+    total_tasks = len(todo_list)
+    completed_tasks = sum(task['completed'] for task in todo_list)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, total_tasks))
+    for task in todo_list:
+        if task['completed']:
+            print("\t{}".format(task['title']))
+
+
+if __name__ == '__main__':
+    fetch_employee_todo_list(argv[1])
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+In this project, we will access employee data via an API to organize and export them to different data structures.
