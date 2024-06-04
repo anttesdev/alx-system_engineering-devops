52a20e2d7ae2b1c95b299400950ca9db8976f49e
Anteneh Tesfaye <anttesdev@gmail.com>
2024-06-04 22:53:01 +0300

    get number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,8 @@ def number_of_subscribers(subreddit):
     """
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Custom"}
+    Custom = "python:subreddit.subscriber.counter:v1.0 (by /u/Anteneh)"
+    headers = {"User-Agent": Custom}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
