5f6b2d8c4d20ce61611b2f5896260ffcc99dd8cb
Anteneh Tesfaye <anttesdev@gmail.com>
2024-06-14 05:17:08 +0300

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,16 @@
+#login with the holberton user and open a file without any error message
+
+#issue is hardfile and soft file open file limit
+exec { 'hard file limit increase':
+  command  => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path     => '/usr/local/bin/:/bin/'
+  provider => shell
+}
+
+exec { 'soft file limit increase':
+  command  => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path     => '/usr/local/bin/:/bin/'
+  provider => shell
+}
+
+
