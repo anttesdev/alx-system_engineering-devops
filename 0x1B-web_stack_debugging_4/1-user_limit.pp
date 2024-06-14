679d2b35ba669eb60caf62fde522ffc51a7242eb
Anteneh Tesfaye <anttesdev@gmail.com>
2024-06-14 05:20:06 +0300

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,13 +4,13 @@
 exec { 'hard file limit increase':
   command  => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path     => '/usr/local/bin/:/bin/'
-  provider => shell
+  provider => shell,
 }
 
 exec { 'soft file limit increase':
   command  => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path     => '/usr/local/bin/:/bin/'
-  provider => shell
+  provider => shell,
 }
 
 
